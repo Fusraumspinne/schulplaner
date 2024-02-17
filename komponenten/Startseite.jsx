b5f45b47e2fb8c1b1bf1960e644a69457bcea2fe
Fusraumspinne <139010069+Fusraumspinne@users.noreply.github.com>
2024-02-17 17:31:16 +0100

    Update Startseite.jsx

--- komponenten/Startseite.jsx
+++ komponenten/Startseite.jsx
@@ -1,8 +1,18 @@
 import Mitteilungen from "@/komponenten/Mitteilungen"
+import Stundenplan from "@/komponenten/Stundenplan"
 
 export default function Startseite() {
 
     return (
-        <Mitteilungen/>
+        <div className="container-fluid">
+            <div className='row mt-4'>
+                <div className="col-9">
+                    <Stundenplan/>
+                </div>
+                <div className="col-3">
+                    <Mitteilungen/>
+                </div>
+            </div>
+        </div>
     );
 }
