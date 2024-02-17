2c29346c7f5b3dd1a8dd749d05b9255a2bf0672b
Fusraumspinne <mjjaeger2002@gmail.com>
2024-02-17 21:08:17 +0100

    first commit

--- komponenten/Wetter.jsx
+++ komponenten/Wetter.jsx
@@ -34,7 +34,7 @@ export default function Mitteilungen() {
             <Card.Body>
                 {loading ? (
                     <>
-                        <p>Laden...</p>
+                        <p>Laden... Hilfe</p>
                         <Button onClick={fetchWetter} variant='secondary'>Aktualisiern</Button>                    
                     </>
                 ) : (
