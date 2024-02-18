323e633b5c113378532f564bb6185f3771cf01da
Fusraumspinne <mjjaeger2002@gmail.com>
2024-02-18 12:51:53 +0100

    scroll fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
 import Layout from "@/komponenten/Layout";
-import "bootstrap/dist/css/bootstrap.min.css" 
-import "@/styles/index.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/index.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return(
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        document.body.style.overflowY = "hidden";
+      } else {
+        document.body.style.overflowY = "auto";
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
     <Layout>
-      <Component	{...pageProps}/>
+      <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
