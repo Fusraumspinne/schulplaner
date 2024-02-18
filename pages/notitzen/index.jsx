e5d46d83c20496bf72c4a45ba9493aeac7902c38
Fusraumspinne <mjjaeger2002@gmail.com>
2024-02-18 17:41:28 +0100

    notizen grundbaugerüst fertig

--- pages/notitzen/index.jsx
+++ pages/notitzen/index.jsx
@@ -1,15 +1,40 @@
-import { Button, Card, Table, InputGroup, CloseButton, Form, DropdownButton, Dropdown } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 
 export default function Notizen() {
+    const [notizen, setNotizen] = useState("");
+    const [geladen, setGeladen] = useState(false)
+
+    useEffect(() => {
+        const storedNotizen = localStorage.getItem('notizen');
+        if (storedNotizen) {
+            setNotizen(JSON.parse(storedNotizen));
+            setGeladen(true)
+        }
+    }, []);
+
+    useEffect(() => {
+        if(geladen){
+            localStorage.setItem('notizen', JSON.stringify(notizen));     
+        }
+    })
+
+    const save = (e) => {
+        setNotizen(e.target.value);
+    }
+
     return (
         <div className='container-fluid'>
             <Card bg='dark' data-bs-theme="dark" className='mx-1 mt-3'>
-            <Card.Body>
-                <textarea className='textarea_notizen' rows="20"></textarea>
-            </Card.Body>
-        </Card>
+                <Card.Body>
+                    <textarea 
+                        value={notizen} 
+                        onChange={save} 
+                        className='textarea_notizen' 
+                        rows="20"
+                    ></textarea>
+                </Card.Body>
+            </Card>
         </div>
-        
     );
 }
