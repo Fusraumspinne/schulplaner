5d2cdb917cc8e36f20bf746f2672a1b4515bc82d
Fusraumspinne <mjjaeger2002@gmail.com>
2024-02-18 19:55:15 +0100

    taschenrechner

--- komponenten/Startseite.jsx
+++ komponenten/Startseite.jsx
@@ -2,6 +2,7 @@ import Mitteilungen from "@/komponenten/Mitteilungen"
 import Stundenplan from "@/komponenten/Stundenplan"
 import Wetter from "@/komponenten/Wetter"
 import Info from "@/komponenten/Info"
+import Taschenrechner from "@/komponenten/Taschenrechner"
 
 export default function Startseite() {
 
@@ -10,6 +11,14 @@ export default function Startseite() {
             <div className='row mt-4'>
                 <div className="col-9 container_start">
                     <Stundenplan/>
+                    <div className="row">
+                        <div className="col-6">
+                            <Taschenrechner/>
+                        </div>
+                        <div className="col-6">
+                            
+                        </div>
+                    </div>
                 </div>
                 <div className="col-3 container_start">
                     <Mitteilungen/>
---
+++ komponenten/Taschenrechner.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import { Card, Button, InputGroup, Form } from 'react-bootstrap';
+
+export default function Taschenrechner() {
+    const [displayValue, setDisplayValue] = useState('');
+
+    const addToDisplay = (value) => {
+        setDisplayValue(displayValue + value);
+    };
+
+    const clearDisplay = () => {
+        setDisplayValue('');
+    };
+
+    const deleteLastCharacter = () => {
+        setDisplayValue(displayValue.slice(0, -1));
+    };
+
+    const calculateResult = () => {
+        try {
+            const result = eval(displayValue);
+            setDisplayValue(result.toString());
+        } catch (error) {
+            setDisplayValue('Error');
+        }
+    };
+
+    return (
+        <Card bg='dark' text='white' className='mx-1 mt-3'>
+            <Card.Title className='d-flex justify-content-center mt-2 fs-2'>
+                Taschenrechner
+            </Card.Title>
+            <Card.Body>
+                <div className='d-flex justify-content-center'>
+                    <div className='rechner'>
+                        <div className='d-flex justify-content-center'>
+                            <div className='container_output'>
+                                <Form.Control className='taschenrechner_output' value={displayValue} readOnly/>
+                            </div>
+                        </div>
+                        <div className='d-flex justify-content-center'>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={clearDisplay}>AC</Button>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={deleteLastCharacter}>DE</Button>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('.')}>.</Button>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('/')}>/</Button>
+                        </div>
+                        <div className='d-flex justify-content-center'>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('7')}>7</Button>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('8')}>8</Button>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('9')}>9</Button>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('*')}>*</Button>
+                        </div>
+                        <div className='d-flex justify-content-center'>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('4')}>4</Button>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('5')}>5</Button>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('6')}>6</Button>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('-')}>-</Button>
+                        </div>
+                        <div className='d-flex justify-content-center'>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('1')}>1</Button>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('2')}>2</Button>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('3')}>3</Button>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('+')}>+</Button>
+                        </div>
+                        <div className='d-flex justify-content-center'>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('00')}>00</Button>
+                            <Button className='taschenrechner_btn' variant='secondary' onClick={() => addToDisplay('0')}>0</Button>
+                            <Button className='result_btn' variant='secondary' onClick={calculateResult}>=</Button>
+                        </div>
+                    </div>
+                </div>
+            </Card.Body>
+        </Card>
+    );
+}
