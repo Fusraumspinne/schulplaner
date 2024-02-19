51fd5656eb925598ba09a61d36084282029103b9
Fusraumspinne <mjjaeger2002@gmail.com>
2024-02-19 21:35:23 +0100

    bug

--- komponenten/Startseite.jsx
+++ komponenten/Startseite.jsx
@@ -3,47 +3,29 @@ import Stundenplan from "@/komponenten/Stundenplan"
 import Wetter from "@/komponenten/Wetter"
 import Info from "@/komponenten/Info"
 import Taschenrechner from "@/komponenten/Taschenrechner"
-import { useState, useEffect } from 'react';
 
 export default function Startseite() {
-    // Zustandsvariablen für die Anzeige der Komponenten
-    const [showStundenplan, setShowStundenplan] = useState(true);
-    const [showTaschenrechner, setShowTaschenrechner] = useState(true);
-    const [showMitteilungen, setShowMitteilungen] = useState(true);
-    const [showWetter, setShowWetter] = useState(true);
-    const [showInfo, setShowInfo] = useState(true);
-
-    useEffect(() => {
-        // Zustände aus dem localStorage abrufen und setzen
-        const savedSettings = JSON.parse(localStorage.getItem('settings'));
-        if (savedSettings) {
-            setShowStundenplan(savedSettings.stundenplan);
-            setShowTaschenrechner(savedSettings.rechner);
-            setShowMitteilungen(savedSettings.mitteilungen);
-            setShowWetter(savedSettings.wetter);
-            setShowInfo(savedSettings.informationen);
-        }
-    }, []);
 
     return (
         <div className="container-fluid">
             <div className='row'>
                 <div className="col-9 container_start pb-5">
-                    {showStundenplan && <Stundenplan/>}
+                    <Stundenplan/>
                     <div className="row">
                         <div className="col-6">
-                            {showTaschenrechner && <Taschenrechner/>}
+                            <Taschenrechner/>
                         </div>
                         <div className="col-6">
+                            
                         </div>
                     </div>
                 </div>
                 <div className="col-3 container_start pb-5">
-                    {showMitteilungen && <Mitteilungen/>}
-                    {showWetter && <Wetter/>}
-                    {showInfo && <Info/>}
+                    <Mitteilungen/>
+                    <Wetter/>
+                    <Info/>
                 </div>
             </div>
         </div>
     );
-}
+}  
